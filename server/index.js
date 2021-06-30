15b389ce7afec9adfccc11e5c123f9f5f460ff17
trak-dev <yann.picaud@gmail.com>
2021-06-30 12:47:49 +0200

    creation architecture

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import routes from "./routes/routes.js";
 
 const app = express();
 dotenv.config();
@@ -9,9 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-app.use("/", (req, res) => {
-  res.send("hello world");
-});
+app.use("/", routes);
 
 mongoose
   .connect(process.env.MONGODB, {
